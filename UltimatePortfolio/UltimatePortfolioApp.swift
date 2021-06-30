f1b13b74c78db553a88dcc9d4d253db392f5a306
Philipp <pd95@users.noreply.github.com>
2021-06-30 09:04:05 +0200

    Designing a great model - CoreData model for Item and Project - DataController class - CloudKit entitlements

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
