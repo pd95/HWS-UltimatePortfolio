090a587fb44d848734cc11e16c79688c2b6adb4f
Philipp <pd95@users.noreply.github.com>
2021-06-30 10:11:16 +0200

    Cleaning up Core Data - Adding Item-CoreDataHelpers and Project-CoreDataHelpers - Replacing null-coalescing with calculated properties - Adding static example objects

---
+++ UltimatePortfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,35 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Philipp on 30.06.21.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+    
+    var itemDetail: String {
+        detail ?? ""
+    }
+    
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+}
