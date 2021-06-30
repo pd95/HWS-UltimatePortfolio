a69b6e3d882079c0b64cfc92471d397b12f864b6
Philipp <pd95@users.noreply.github.com>
2021-06-30 13:38:34 +0200

    Adding a section header - New ProjectHeaderView view to show project progress and an edit button - Adding asset catalog with custom colors.

--- UltimatePortfolio/ProjectsView.swift
+++ UltimatePortfolio/ProjectsView.swift
@@ -27,7 +27,7 @@ struct ProjectsView: View {
         NavigationView {
             List {
                 ForEach(projects.wrappedValue) { project in
-                    Section(header: Text(project.projectTitle)) {
+                    Section(header: ProjectHeaderView(project: project)) {
                         ForEach(project.projectItems) { item in
                             ItemRowView(item: item)
                         }
