88c2002f437957916ca01bbffa17de5c5d2fb30e
Philipp <pd95@users.noreply.github.com>
2022-02-26 13:56:18 +0100

    Align `CloudError` implementation to Pauls variant (using `ExpressibleByStringInterpolation` protocol and a separate Error-CloudKitMessages.swift

--- UltimatePortfolio/Activities/CloudError.swift
+++ UltimatePortfolio/Activities/CloudError.swift
@@ -5,40 +5,13 @@
 //  Created by Philipp on 14.09.21.
 //
 
-import Foundation
-import CloudKit
+import SwiftUI
 
-struct CloudError: Identifiable {
-    let id = UUID()
-    var error: CKError
+struct CloudError: Identifiable, ExpressibleByStringInterpolation {
+    var id: String { message }
+    var message: String
 
-    var message: String {
-        switch error.code {
-        case .badDatabase, .badContainer, .invalidArguments:
-            return "A fatal error occurred: \(error.localizedDescription)"
-        case .networkFailure, .networkUnavailable, .serverResponseLost, .serviceUnavailable:
-            return "There was a problem communicating with iCloud; please check your network connection and try again."
-        case .notAuthenticated:
-            return "There was a problem with your iCloud account; please check that you're logged in to iCloud."
-        case .requestRateLimited:
-            return "You've hit iCloud's rate limit; please wait a moment then try again."
-        case .quotaExceeded:
-            return "You've exceeded your iCloud quota; please clear up some space then try again."
-        default:
-            return "An unknown error occurred: \(error.localizedDescription)"
-        }
-    }
-
-    init?(_ error: Error) {
-        guard let error = error as? CKError else {
-            return nil
-        }
-        self.error = error
-    }
-}
-
-extension Error {
-    func getCloudKitError() -> CloudError? {
-        CloudError(self)
+    init(stringLiteral value: String) {
+        self.message = value
     }
 }
---
+++ UltimatePortfolio/Extensions/Error-CloudKitMessages.swift
@@ -0,0 +1,32 @@
+//
+//  Error-CloudKitMessages.swift
+//  UltimatePortfolio
+//
+//  Created by Philipp on 26.02.22.
+//
+
+import CloudKit
+import Foundation
+
+extension Error {
+    func getCloudKitError() -> CloudError {
+        guard let error = self as? CKError else {
+            return "An unknown error occurred: \(self.localizedDescription)"
+        }
+
+        switch error.code {
+        case .badDatabase, .badContainer, .invalidArguments:
+            return "A fatal error occurred: \(error.localizedDescription)"
+        case .networkFailure, .networkUnavailable, .serverResponseLost, .serviceUnavailable:
+            return "There was a problem communicating with iCloud; please check your network connection and try again."
+        case .notAuthenticated:
+            return "There was a problem with your iCloud account; please check that you're logged in to iCloud."
+        case .requestRateLimited:
+            return "You've hit iCloud's rate limit; please wait a moment then try again."
+        case .quotaExceeded:
+            return "You've exceeded your iCloud quota; please clear up some space then try again."
+        default:
+            return "An unknown error occurred: \(self.localizedDescription)"
+        }
+    }
+}
